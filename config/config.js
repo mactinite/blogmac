7868a1d7249eae9b3e09efda62dd9abe922999aa
Hunter MacDonald <mactinite77@gmail.com>
2017-09-26 18:21:00 -0700

    omg forreal

---
+++ config/config.js
@@ -0,0 +1,8 @@
+var mongoPwd = "Damn!5473",
+    mongoUser = "mactinite";
+
+
+module.exports = {
+    url: 'mongodb://' + mongoUser + ':' + mongoPwd + '@ds029595.mlab.com:29595/mactinite-blog',
+    cookieSecret : "hoEZZQS0EShrt4im5MfV"
+};
